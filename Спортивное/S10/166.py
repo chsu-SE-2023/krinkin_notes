f69ba6efd29761117d5bf76069327f6b9ca1ccad
OleSTEEP <krinkinoleg@gmail.com>
2025-04-15 09:04:55 +0300

    "Курс 2" is now fully transfered

--- Спортивное/S10/166.py
+++ Спортивное/S10/166.py
@@ -5,18 +5,19 @@ q_year = Queue()
 q_year.put(start)
 
 for i in range(2, year+1):
-    if i % 3 == 0: count -= q_year.get()
+    if i % 4 == 0: count -= q_year.get()
     y_count = 0
-    while start != 0:
-        if start >= 5:
-            start -= 5
-            y_count += 9
-        elif start >= 3:
-            start -= 3
-            y_count += 5
-        else:
-            y_count -= 4
-            start += 2
+    if start > 2:
+        while start != 0:
+            if start % 3 == 0:
+                start -= 3
+                y_count += 5
+            elif start % 5 == 0:
+                start -= 5
+                y_count += 9
+            else:
+                start += 3
+                y_count -= 5
 
     q_year.put(y_count)
     count += y_count
