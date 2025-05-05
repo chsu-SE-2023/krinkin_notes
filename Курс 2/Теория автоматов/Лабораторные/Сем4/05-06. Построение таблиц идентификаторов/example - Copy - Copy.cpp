1a2e7b4feae596a7de2e5a7e0dcf1a57c19726c6
OleSTEEP <krinkinoleg@gmail.com>
2025-05-05 13:31:20 +0300

    recent lab changes: 2025-05-05 13:31:20

--- Курс 2/Теория автоматов/Лабораторные/Сем4/05-06. Построение таблиц идентификаторов/example - Copy - Copy.cpp
+++ Курс 2/Теория автоматов/Лабораторные/Сем4/05-06. Построение таблиц идентификаторов/example - Copy - Copy.cpp
@@ -1,2 +1,14 @@
----5
-//std::cout << i++ << ++i << i-- << --i << std::endl;
+314.56
+31.56E+02
+32.33E
+145.16A7
+<<<<<
+++-+==
+3_nomer
+nomer_3
+4nomer
+--=+<=>=>>
+;;;
+int_1
+int char
+int_char
