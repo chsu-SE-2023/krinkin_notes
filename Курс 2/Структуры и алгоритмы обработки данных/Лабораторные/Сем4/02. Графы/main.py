90e322786c6871f34736d06dbc89dc888a375307
OleSTEEP <krinkinoleg@gmail.com>
2025-05-30 12:43:07 +0300

    recent lab changes: 2025-05-30 12:43:07

--- Курс 2/Структуры и алгоритмы обработки данных/Лабораторные/Сем4/02. Графы/main.py
+++ Курс 2/Структуры и алгоритмы обработки данных/Лабораторные/Сем4/02. Графы/main.py
@@ -1,21 +1,37 @@
 import matplotlib.pyplot as pyplot
 import networkx
 
+window = 100
+
 def draw_graph(node_list: list) -> None:
+    """Функция, рисующая граф на экране
+
+    Аргументы:
+    node_list -- список нод формата [[a, b], [b, c]]
+    """
+    global window
+    pyplot.figure(window)
+
     graph = networkx.DiGraph()
 
     for nodes in node_list:
         graph.add_edge(nodes[0].strip(), nodes[1].strip())
 
-    pos = networkx.spring_layout(graph, k=10)
+    pos = networkx.spring_layout(graph, k=10, seed=0)
     networkx.draw_networkx_nodes(graph, pos, node_size=500, node_color="yellow")
     networkx.draw_networkx_edges(graph, pos, width=2, arrowsize=25)
     networkx.draw_networkx_labels(graph, pos, font_size=12)
 
-    pyplot.tight_layout()
-    pyplot.show()
+    window *= (window // 100) + 1
 
 def fix_graph(node_list: list) -> list:
+    """Функция, исправляющая граф
+
+    Аргументы:
+    node_list -- список нод формата [[a, b], [b, c]]
+    Возвращает:
+    fixed_edges -- список нод формата [[a, b], [b, c]]
+    """
     fixed_edges = []
     for nodes in node_list:
         if int(nodes[0]) < int(nodes[1]):
@@ -30,4 +46,8 @@ if __name__ == "__main__":
     f_edges = file[1:]
     f_nodes = [edge.strip().split(" ") for edge in f_edges]
 
+    draw_graph(f_nodes)
     draw_graph(fix_graph(f_nodes))
+
+    pyplot.tight_layout()
+    pyplot.show()
