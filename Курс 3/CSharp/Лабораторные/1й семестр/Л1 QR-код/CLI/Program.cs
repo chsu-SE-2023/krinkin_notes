1ae8ffe5915ab81cb8767f5d939ad6f818dfa529
OleSTEEP <krinkinoleg@gmail.com>
2025-10-14 00:36:54 +0300

    vault backup: 2025-10-14 00:36:54

--- Курс 3/CSharp/Лабораторные/1й семестр/Л1 QR-код/CLI/Program.cs
+++ Курс 3/CSharp/Лабораторные/1й семестр/Л1 QR-код/CLI/Program.cs
@@ -1,9 +1,21 @@
 ﻿
 Console.Write("Введите текст: ");
 var input = Console.ReadLine();
+
+
+Console.Write("1. Текстовый\n2. Только рисунок\n3. Рисунок и текст\nТип вывода информации: ");
+var type = Int32.Parse(Console.ReadLine());
+
 if (input != null)
 {
     QRCode code = new(input);
-    Console.WriteLine(code.QRData.Data);
-    Console.WriteLine(code.ToString());
+    string data = type switch
+    {
+        1 => code.ToString(),
+        2 => code.QRData.Data,
+        3 => code.QRData.Data + "\n" + code.ToString(),
+        _ => ""
+    };
+
+    Console.WriteLine(data);
 }
