a8485bd347e71404dc75c628f75836c414821c7a
OleSTEEP <krinkinoleg@gmail.com>
2026-01-15 16:43:27 +0300

    vault backup: 2026-01-15 16:43:27

--- Курс 3/Базы данных/Лабораторные/06. Взаимодействие с БД Access/DataBaseApp/Queries.cs
+++ Курс 3/Базы данных/Лабораторные/06. Взаимодействие с БД Access/DataBaseApp/Queries.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-﻿
 namespace DataBaseApp
 {
     internal class Queries
@@ -59,65 +57,3 @@ SELECT Фамилия, ДатаРождения FROM student2;";
         }
     }
 }
-=======
-﻿
-namespace DataBaseApp
-{
-    internal class Queries
-    {
-        /// <summary>
-        /// Запрос студентов с указанием фамилии и даты рождения
-        /// </summary>
-        /// <returns>SQL строка</returns>
-        public static string SurnameDate()
-        {
-            return 
-@"SELECT Фамилия, ДатаРождения FROM student1
-    UNION
-SELECT Фамилия, ДатаРождения FROM student2;";
-        }
-
-        /// <summary>
-        /// Запрос студентов-отличников по математике
-        /// </summary>
-        /// <returns>SQL строка</returns>
-        public static string GoodOnesMath()
-        {
-            return 
-@"SELECT * FROM (
-    SELECT * FROM student1
-    UNION
-    SELECT * FROM student2
-) WHERE Математика = 5;";
-        }
-
-        /// <summary>
-        /// Запрос студентов-отличников по всем предметам
-        /// </summary>
-        /// <returns>SQL строка</returns>
-        public static string GoodOnes()
-        {
-            return 
-@"SELECT * FROM (
-    SELECT * FROM student1
-    UNION
-    SELECT * FROM student2
-) WHERE Математика = 5 AND Информатика = 5 AND Иностранный = 5;";
-        }
-
-        /// <summary>
-        /// Запрос студентов, чей возраст старше 20 лет
-        /// </summary>
-        /// <returns>SQL строка</returns>
-        public static string OldOnes()
-        {
-            return 
-@"SELECT * FROM (
-    SELECT * FROM student1
-    UNION
-    SELECT * FROM student2
-) WHERE DateDiff(""yyyy"",ДатаРождения,Date()) >= 20;";
-        }
-    }
-}
->>>>>>> d617c3e (vault backup: 2025-12-22 13:02:11)
